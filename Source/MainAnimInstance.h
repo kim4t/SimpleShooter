2cd5002730bdc4def745320a46db6187e23f60da
Taeho Kim <kim4t@cmich.edu>
2021-01-31 14:27:10 -0500

    Add files via upload

---
+++ Source/MainAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Movement)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Movement)
+	bool bIsInAir;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Movement)
+	class APawn* Pawn;
+
+
+	
+};
